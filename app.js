9b89512af9cd18784ee950c1feb10a793e7d1ce9
heelsboroharry <74061248+heelsboroharry@users.noreply.github.com>
2023-11-11 21:31:23 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+function loadHomePage() {
+    document.getElementById('content').innerHTML = '<object type="text/html" data="home.html" ></object>';
+}
+
+function loadChartsPage() {
+    document.getElementById('content').innerHTML = '<object type="text/html" data="charts.html" ></object>';
+}
+
+function loadComparisonPage() {
+    document.getElementById('content').innerHTML = '<object type="text/html" data="comparison.html" ></object>';
+}
+
+function loadEditPage() {
+    // Implement logic to load the page where you can edit your portfolio
+    alert('Feature under development.');
+}
